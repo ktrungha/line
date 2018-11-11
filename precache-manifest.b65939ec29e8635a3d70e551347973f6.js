82b9432d0893ae55986d1bab7d5be90a6f4c8f6a
Trung Ha <ktrungha@hotmail.com>
2018-11-11 21:16:02 +0700

    initial commit

---
+++ precache-manifest.b65939ec29e8635a3d70e551347973f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84525db316e4e2f5353c",
+    "url": "/static/js/main.84525db3.chunk.js"
+  },
+  {
+    "revision": "92f1908976a8260ec2ae",
+    "url": "/static/js/1.92f19089.chunk.js"
+  },
+  {
+    "revision": "84525db316e4e2f5353c",
+    "url": "/static/css/main.bae89f54.chunk.css"
+  },
+  {
+    "revision": "53bc35f0c7de1aa0e5186d35c1f22a98",
+    "url": "/index.html"
+  }
+];
